bdab6131c4d0c4859b58bd68ba6767a4b5b2ebcd
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2024-10-12 19:58:30 -0600

    RepositorioLab
    Se inicializa un repositorio para trabajar en el labortorio de ajadrez

---
+++ src/lab_ajedrez3/pkg0/Lab_Ajedrez30.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_ajedrez3.pkg0;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Lab_Ajedrez30 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
